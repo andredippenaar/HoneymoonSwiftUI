ac324fcb279a95f2596107c315a864889e0d774c
andredippenaar <andre.pierre.dippenaar@gmail.com>
2022-05-17 10:56:55 +0200

    Initial Commit

---
+++ HoneymoonSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HoneymoonSwiftUI
+//
+//  Created by Andre Dippenaar on 2022/05/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HoneymoonSwiftUI/HoneymoonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonSwiftUIApp.swift
+//  HoneymoonSwiftUI
+//
+//  Created by Andre Dippenaar on 2022/05/17.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

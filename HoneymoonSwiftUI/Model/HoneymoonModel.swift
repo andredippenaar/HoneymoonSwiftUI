3f81975694851671dd1c72e0fc558eb16fcda9a7
andre-dippenaar <andre@nona.digital>
2022-11-23 10:53:11 +0200

    Built layout for individual honeymoon card

---
+++ HoneymoonSwiftUI/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  HoneymoonSwiftUI
+//
+//  Created by Andre Dippenaar on 2022/11/23.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
---
+++ HoneymoonSwiftUI/View/CardView.swift
@@ -0,0 +1,58 @@
+//
+//  CardView.swift
+//  HoneymoonSwiftUI
+//
+//  Created by Andre Dippenaar on 2022/11/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    // MARK: - PROPERTIES
+    
+    let id =  UUID()
+    var honeymoon: Destination
+    
+    var body: some View {
+        Image(honeymoon.image)
+            .resizable()
+            .cornerRadius(24)
+            .scaledToFit()
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .overlay(
+                VStack(alignment: .center, spacing: 12) {
+                    Text(honeymoon.place.uppercased())
+                        .foregroundColor(Color.white)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .shadow(radius: 1)
+                        .padding(.horizontal, 18)
+                        .padding(.vertical, 4)
+                        .overlay(
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(height: 1),
+                        alignment: .bottom
+                        )
+                    Text(honeymoon.country.uppercased())
+                        .foregroundColor(Color.black)
+                        .font(.footnote)
+                        .fontWeight(.bold)
+                        .frame(minWidth: 85)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 5)
+                        .background(Capsule().fill(Color.white))
+                }
+                .frame(minWidth: 280)
+                .padding(.bottom, 50),
+                alignment: Alignment.bottom
+            )
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(honeymoon: honeymoonData[1])
+            .previewLayout(.fixed(width: 375, height: 600))
+    }
+}
